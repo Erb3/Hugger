ff7556752e362097dec62d38f774c71b6ac90ea1
Erb3 <49862976+Erb3@users.noreply.github.com>
2022-11-09 17:05:53 +0100

    Add useless check if /hug is registered in plugin.yml

--- src/main/java/github/erb3/plugin/Hugger/Main.java
+++ src/main/java/github/erb3/plugin/Hugger/Main.java
@@ -2,6 +2,7 @@ package github.erb3.plugin.Hugger;
 
 import github.erb3.plugin.Hugger.command.Hug;
 import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -15,10 +16,18 @@ public class Main extends JavaPlugin {
         this.conf.createConfig();
 
         if (!this.conf.getBoolean("enabled")) {
-            Bukkit.getLogger().info("Hugging has been disabled in its config. You can re-enable it in plugins/hugger/config.yml");
+            Bukkit.getLogger().warning("Hugging has been disabled in its config. You can re-enable it in plugins/hugger/config.yml");
             this.getServer().getPluginManager().disablePlugin(this);
         } else {
-            this.getCommand("hug").setExecutor(new Hug(this.conf));
+            PluginCommand hugCmd = this.getCommand("hug");
+
+            if (hugCmd == null ) {
+                Bukkit.getLogger().warning("/Hug not registered in plugin.yml? Please contact the mod developers. This is not supposed to happen!");
+                this.getServer().getPluginManager().disablePlugin(this);
+                return;
+            }
+
+            hugCmd.setExecutor(new Hug(this.conf));
         }
 
     }
