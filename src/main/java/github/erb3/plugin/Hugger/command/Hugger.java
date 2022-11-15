ced900cba21c79ca03b5a247d388ab61a00c799b
Erb3 <49862976+Erb3@users.noreply.github.com>
2022-11-15 16:11:21 +0100

    Remove unnecessary help line.

--- src/main/java/github/erb3/plugin/Hugger/command/Hugger.java
+++ src/main/java/github/erb3/plugin/Hugger/command/Hugger.java
@@ -65,7 +65,6 @@ public class Hugger implements CommandExecutor {
     public boolean helpCommand(CommandSender sender, Command cmd, String label, String[] args) {
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "\n        &8<< &3Hugger &7by &6Erb3 & Contributors &8>>" +
                 "\n  &3>> &7/hug &d[player]" +
-                "\n  &3>> &7/hugger &d[config/help]" +
                 "\n  &3>> &7/hugger config reload" +
                 "\n  &3>> &7/hugger help\n "));
         return true;
