25c806f99fc1564de97b4d7d20665901e2add2f0
Erb3 <49862976+Erb3@users.noreply.github.com>
2022-11-15 17:09:26 +0100

    Create click and hover events for help menu.

--- src/main/java/github/erb3/plugin/Hugger/command/Hugger.java
+++ src/main/java/github/erb3/plugin/Hugger/command/Hugger.java
@@ -2,6 +2,9 @@ package github.erb3.plugin.Hugger.command;
 
 import github.erb3.plugin.Hugger.Main;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.*;
+import net.md_5.bungee.api.chat.hover.content.Content;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -63,10 +66,23 @@ public class Hugger implements CommandExecutor {
     }
 
     public boolean helpCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "\n        &8<< &3Hugger &7by &6Erb3 & Contributors &8>>" +
-                "\n  &3>> &7/hug &d[player]" +
-                "\n  &3>> &7/hugger config reload" +
-                "\n  &3>> &7/hugger help\n "));
+
+        TextComponent hugPlayer = new TextComponent(ChatColor.translateAlternateColorCodes('&',"\n  &3>> &7/hug &d[player]"));
+        TextComponent configReload = new TextComponent(ChatColor.translateAlternateColorCodes('&',"\n  &3>> &7/hugger config reload"));
+        TextComponent help = new TextComponent(ChatColor.translateAlternateColorCodes('&',"\n  &3>> &7/hugger help\n "));
+
+        hugPlayer.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/hug "));
+        configReload.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/hugger config reload"));
+        help.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/hugger help"));
+
+        HoverEvent tabCompletion = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click for autocompletion"));
+        hugPlayer.setHoverEvent(tabCompletion);
+        configReload.setHoverEvent(tabCompletion);
+        help.setHoverEvent(tabCompletion);
+
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "\n        &8<< &3Hugger &7by &6Erb3 & Contributors &8>>"));
+        sender.spigot().sendMessage(hugPlayer, configReload, help);
+
         return true;
     }
 }
