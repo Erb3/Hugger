a55b60ba7542ad0b5e4a34203eb5d5e83915c2d3
Erb3 <49862976+Erb3@users.noreply.github.com>
2022-11-20 14:28:12 +0100

    Javadoc(/hugger): Added more documentation in the Effect interface

--- src/main/java/github/erb3/plugin/Hugger/effects/Effect.java
+++ src/main/java/github/erb3/plugin/Hugger/effects/Effect.java
@@ -5,12 +5,19 @@ import org.bukkit.entity.Player;
 
 public interface Effect {
     /**
-     * Function that executes when someone gets hugged
+     * Function that executes when someone gets hugged. It should not return anything.
+     * Both parameters are required.
+     * @param from The sender of the hug. The one that hugs.
+     * @param to The person that is getting hugged
+     * @since 1.0.0
      */
     void runEffect(CommandSender from, Player to);
 
     /**
-     * String that contains the effect name. This is used in config file
+     * This function returns a string, that contains the effect name.
+     * It is used in the config file to enable/configure the effect.
+     * @return The name of the effect
+     * @since 1.0.0
      */
     String getName();
 }
